758dcda17e6e63d752331065f516b8b959e8c932
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-11-03 21:38:58 +0900

    api 익히기

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,17 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-tem";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //데이터를 그대로 내려줌
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //"hello spring"
     }
 }
